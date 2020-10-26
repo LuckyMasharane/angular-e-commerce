ecf59856459ac63396b62c29f60e38c3b7b3b1b3
LuckyMasharane <lmasharane@gmail.com>
2020-10-26 06:15:32 +0200

    1 file modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductComponent, LoginComponent, RegisterComponent, AddProductComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
