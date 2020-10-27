8684adc2a0c63d3c7c0d1cf8ecf1a0c6c6da5bd3
LuckyMasharane <lmasharane@gmail.com>
2020-10-27 07:28:17 +0200

    1 file modified

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -12,9 +12,9 @@ export class ProductComponent implements OnInit {
   ngOnInit() {
   }
   product = [
-    {picture:"",price:"145.00",name:"T-shirt"},
-    {picture:"",price:"499.00",name:"Jeans"},
-    {picture:"",price:"1120",name:"All Star"},
+    {picture:"https://images.unsplash.com/photo-1523381294911-8d3cead13475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",price:"145.00",name:"T-shirt"},
+    {picture:"https://images.unsplash.com/photo-1576995853123-5a10305d93c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",price:"499.00",name:"Jeans"},
+    {picture:"https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",price:"1120",name:"Adidas sneakers"},
 
   ]
 }
