d9ac0751a63291944018363cacc6774c32c1d34f
LuckyMasharane <lmasharane@gmail.com>
2020-10-27 08:01:58 +0200

    1 file modified

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../product/product.component';
-
+import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 import { AddProductComponent } from '../add-product/add-product.component';
 
 const appRouting:Routes = [
-  {path:"",component:ProductComponent},
-  {path:"login",component:LoginComponent},
-  {path:"register",component:RegisterComponent},
-  {path:"add-product",component:AddProductComponent}
+  { path:"", component:ProductComponent },
+  { path:"login", component:LoginComponent },
+  { path:"register", component:RegisterComponent },
+  { path:"add-product", component:AddProductComponent }
 ]
 
 @NgModule({
